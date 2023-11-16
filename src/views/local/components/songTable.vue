441f90fbfab58b2b1c43b799f5f2209feef89055
lichong <wslcwslc@126.com>
2023-11-16 14:29:45 +0800

    feat: 播放列表

--- src/views/local/components/songTable.vue
+++ src/views/local/components/songTable.vue
@@ -1,11 +1,37 @@
 <template>
-  <el-table
-    border
-    highlight-current-row
-    style="width: 100%"
-    :data="state.tableData"
-  >
-    <el-table-column prop="name" label="歌曲名" width="390" />
+  <el-table highlight-current-row style="width: 100%" :data="state.tableData">
+    <el-table-column prop="name" label="歌曲名" width="390">
+      <template #default="{ row }">
+        <div class="song-container flex">
+          <div class="left-container flex">
+            <div class="title ellipsis">{{ row.name }}</div>
+            <div class="mv no-shrink flex-center click-active">MV</div>
+            <div class="playing no-shrink flex-center">
+              <i class="ri-rhythm-line"></i>
+            </div>
+          </div>
+          <div class="right-container flex no-shrink" title="播放">
+            <div class="icon-container click-active">
+              <i class="ri-play-line"></i>
+            </div>
+            <div class="icon-container click-active">
+              <i
+                v-if="row.hasLiked"
+                class="ri-heart-3-fill"
+                title="取消喜欢"
+              ></i>
+              <i v-else class="ri-heart-3-line" title="喜欢"></i>
+            </div>
+            <div class="icon-container click-active" title="下载">
+              <i class="ri-download-2-line"></i>
+            </div>
+            <div class="icon-container click-active" title="更多">
+              <i class="ri-apps-line"></i>
+            </div>
+          </div>
+        </div>
+      </template>
+    </el-table-column>
     <el-table-column prop="empty"></el-table-column>
     <el-table-column
       prop="singer"
@@ -29,9 +55,52 @@ import { reactive } from 'vue';
 const state = reactive({
   tableData: [
     { name: '起风了', singer: '这是一个歌', time: '02:55:55' },
-    { name: '吹灭小山河', singer: '这是两个歌手', time: '02:44' }
+    {
+      name: '吹吹灭小山河吹灭小山河吹灭小山河吹灭小山河吹灭小山河灭小山河',
+      singer: '这是两个歌手',
+      time: '02:44'
+    }
   ]
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.song-container {
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  .left-container {
+    width: 0;
+    padding-right: 30px;
+    flex-grow: 1;
+    align-items: center;
+    .mv {
+      height: 12px;
+      padding: 0 2px;
+      margin-left: 4px;
+      border: 1px solid $primaryColor;
+      font-size: 10px;
+      color: $primaryColor;
+      line-height: 10px;
+      cursor: pointer;
+    }
+    .playing {
+      margin-left: 4px;
+      font-size: 12px;
+      color: $primaryColor;
+    }
+  }
+  .right-container {
+    align-items: center;
+    .icon-container {
+      margin-left: 18px;
+      font-size: 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+        font-size: 18px;
+      }
+    }
+  }
+}
+</style>
